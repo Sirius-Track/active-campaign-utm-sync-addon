4c9b06c025190a3131340f44fbaa121b33e58dd0
DiogoJorge1401 <diogojorge1401@gmail.com>
2023-06-02 02:53:03 -0300

    ♻️ refactor: remove code  doesn't work

--- src/server/shared/finalizeCustomFieldsMapping.ts
+++ src/server/shared/finalizeCustomFieldsMapping.ts
@@ -27,10 +27,6 @@ export const finalizeCustomFieldsMapping = (formData: {
 
   updateMissingValuesWithActiveCampaignData();
 
-  SpreadsheetApp.getUi()
-    .createMenu('ActiveCampaign UTM Sync')
-    .addItem('Sync ActiveCampaign', 'syncActiveCampaign');
-
   ScriptApp.newTrigger('onFormSubmit')
     .forSpreadsheet(SpreadsheetApp.getActiveSpreadsheet())
     .onFormSubmit()
