8518a0a5a734a1c29b409720f47c832b511f53ce
DiogoJorge1401 <diogojorge1401@gmail.com>
2023-06-02 03:07:53 -0300

    ♻️ refactor: handle request exceptions

--- src/server/shared/getContactId.ts
+++ src/server/shared/getContactId.ts
@@ -3,20 +3,24 @@ export const getContactId = (
   apiToken: string,
   email: string
 ) => {
-  const options = {
-    method: 'GET',
-    headers: {
-      'Api-Token': apiToken,
-    },
-  };
+  try {
+    const options = {
+      method: 'GET',
+      headers: {
+        'Api-Token': apiToken,
+      },
+    };
 
-  const response = UrlFetchApp.fetch(
-    `${apiUrl}/api/3/contacts?email=${email}`,
-    options
-  );
-  const json = JSON.parse(response.getContentText());
+    const response = UrlFetchApp.fetch(
+      `${apiUrl}/api/3/contacts?email=${email}`,
+      options
+    );
+    const json = JSON.parse(response.getContentText());
 
-  if (json.contacts && json.contacts.length > 0) return json.contacts[0].id;
+    if (json.contacts && json.contacts.length > 0) return json.contacts[0].id;
 
-  return null;
+    return null;
+  } catch (error) {
+    return null;
+  }
 };
